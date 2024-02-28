14f1d0e3f4ca4915336722157208003f95bbb32a
ajidstorm <apexes_09kooks@icloud.com>
2024-02-28 18:26:53 -0500

    Added usage stats view but not working because permissions broken

--- app/src/main/java/com/example/addictionfighter2/MainActivity.java
+++ app/src/main/java/com/example/addictionfighter2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.addictionfighter2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonUsageStats = findViewById(
+                R.id.button_usage_stats
+        );
+        buttonUsageStats.setOnClickListener(view -> {
+            Intent usageStatsIntent = new Intent(
+                    getApplicationContext(), UsageStatsActivity.class
+            );
+            startActivity(usageStatsIntent);
+        });
+
     }
+
+
 }
